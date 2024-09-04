9f2d15dc9261a5d7f32758bea521c81d7691c59a
programmergautam <gautm.das@gmail.com>
2024-09-04 21:28:01 +0530

    removed unnecessary import

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import emailService from "../utils/emailService";
 
